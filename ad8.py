2d4c060c3841a548cb2be8ce4a03e599f9868529
Nishan Soni <nishan.soni2@gmail.com>
2024-12-07 23:17:44 -0700

    day 8

---
+++ ad8.py
@@ -0,0 +1,49 @@
+import collections
+
+class Solution:
+
+    def get_input(self):
+        
+        positions = collections.defaultdict(list)
+        grid = []
+
+        with open("input8.txt") as file:
+            for line in file:
+                grid.append(list(line.strip("\n")))
+        
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] not in (".", "#"):
+                    positions[grid[r][c]].append((r, c))
+
+        return positions, grid
+    
+    def solve(self, positions, grid):
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        unique_antinodes = set()
+
+        for antenna_key in positions:
+            for i in range(len(positions[antenna_key])):
+                # Caluclate distance between given node and all others
+                for j in range(len(positions[antenna_key])):
+                    if j == i:
+                        continue
+                    # Find position of anti node and add it to answer if in bounds
+                    delta_r, delta_c = positions[antenna_key][j][0] - positions[antenna_key][i][0], positions[antenna_key][j][1] - positions[antenna_key][i][1]
+
+                    anitnode_r, antinode_c = delta_r + positions[antenna_key][j][0], delta_c + positions[antenna_key][j][1]
+
+                    if not(anitnode_r < 0 or antinode_c < 0 or anitnode_r >= ROWS or antinode_c >= COLS or (anitnode_r, antinode_c) in unique_antinodes):
+                        unique_antinodes.add((anitnode_r, antinode_c))
+        
+        return len(unique_antinodes)
+        
+s = Solution()
+
+print(s.solve(*s.get_input()))
